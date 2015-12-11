d71b6efc4e285e22c30f0a6b1606ae87814a46e7
Danny Anderson <danny@danny-anderson.com>
2015-12-11 10:38:16 -0600

    Add gentags alias to update ctags for app+engines

--- bash/shell.sh
+++ bash/shell.sh
@@ -1,4 +1,4 @@
 # Use vim as the default editor
 export EDITOR='vim'
 
-alias update_ctags="ctags -R --languages=ruby --exclude=.git --exclude=log . \$(bundle list --paths)"
+alias gentags="ctags -R --languages=ruby --exclude=.git --exclude=log --exclude=_!not-app-related . \$(for gemfile in `ls */Gemfile`; do BUNDLE_GEMFILE=$gemfile bundle list --paths; done)"
