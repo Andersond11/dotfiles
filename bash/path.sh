6611f8855673d0a3d785ab4d03095c21c12a15ba
Danny Anderson <danny@danny-anderson.com>
2018-11-20 09:22:33 -0600

    Remove php56 from path, since I'm not using it

--- bash/path.sh
+++ bash/path.sh
@@ -1,6 +1,5 @@
 export PATH="/usr/local/bin:$PATH" # Make sure homebrew installed apps are used before default apps
 export PATH="/usr/local/sbin:$PATH"
-export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 export PATH="$HOME/.bin:$PATH"
 export PATH="$PATH:/usr/local/lib/node_modules"
